046097bf5d180b1e9bafe21dfad7bc7c5e4a9d8b
Wonder <64184513+NeverWonderLand@users.noreply.github.com>
2023-03-29 15:52:34 -0400

    Update wildcrawl.sh

--- wildcrawl.sh
+++ wildcrawl.sh
@@ -52,7 +52,7 @@ touch domains.txt
 # Extract all domains from the unique_urls.txt file, remove duplicates, and sort them
 cat unique_urls.txt | awk -F/ '{print $3}' | sort -u >> domains.txt
 
-grep -Ev 'wikipedia|jquery|android|euronews|forbes|foreignaffairs|france24|huffingtonpost|economist|theguardian|dailytimes|washingtonpost|theglobeandmail|creativecommons|foreignpolicy|reuters|maxcdn|thefrontierpost|theintercept|weibo|wordpress|aljazeera|amnesty|bloomberg|bbc|cnn|dailymail|businessinsider|facebook|meta|messenger|fbcdn|bulletin|googletagmanager|oculus|twitter|youtube|instagram|google|apple|microsoft|twimg|telegram|t\.me|cloudflare|jsdelivr|youtu|linkedin' domains.txt > filtered_domains.txt
+grep -Ev 'wikipedia|waze|goo\.gl|aparat\.com|jquery|android|euronews|forbes|foreignaffairs|france24|huffingtonpost|economist|theguardian|dailytimes|washingtonpost|theglobeandmail|creativecommons|foreignpolicy|reuters|maxcdn|thefrontierpost|theintercept|weibo|wordpress|aljazeera|amnesty|bloomberg|bbc|cnn|dailymail|businessinsider|facebook|meta|messenger|fbcdn|bulletin|googletagmanager|oculus|twitter|youtube|instagram|google|apple|microsoft|twimg|telegram|t\.me|cloudflare|jsdelivr|youtu|linkedin' domains.txt > filtered_domains.txt
 
 echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
 echo $GREEN [x] Extracting records of domains.. $RESET
