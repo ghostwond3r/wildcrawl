7fb5324073df145cbd6ad99b21e751bb6f931e36
Wonder <64184513+NeverWonderLand@users.noreply.github.com>
2023-03-29 16:02:59 -0400

    Update wildcrawl.sh

--- wildcrawl.sh
+++ wildcrawl.sh
@@ -30,12 +30,8 @@ sort urls.txt | uniq > unique_urls.txt
 # Extract all files and separate them from the test
 touch files.txt
 
-# Loop through each file extension
 for ext in js txt pdf doc docx csv zip jpg png rar apk; do
-  # Find all links in the unique_urls.txt file with the current extension and save them to files.txt
   grep -o "http.*\.$ext" unique_urls.txt >> files.txt
-
-  # Remove all links with the current extension from the unique_urls.txt file
   sed -i "/\.$ext\b/d" unique_urls.txt
 done
 
@@ -49,7 +45,7 @@ echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
 
 touch domains.txt
 
-# Extract all domains from the unique_urls.txt file, remove duplicates, and sort them
+# Extract all domains, remove duplicates, and sort them
 cat unique_urls.txt | awk -F/ '{print $3}' | sort -u >> domains.txt
 
 grep -Ev 'wikipedia|waze|goo\.gl|aparat\.com|jquery|android|euronews|forbes|foreignaffairs|france24|huffingtonpost|economist|theguardian|dailytimes|washingtonpost|theglobeandmail|creativecommons|foreignpolicy|reuters|maxcdn|thefrontierpost|theintercept|weibo|wordpress|aljazeera|amnesty|bloomberg|bbc|cnn|dailymail|businessinsider|facebook|meta|messenger|fbcdn|bulletin|googletagmanager|oculus|twitter|youtube|instagram|google|apple|microsoft|twimg|telegram|t\.me|cloudflare|jsdelivr|youtu|linkedin' domains.txt > filtered_domains.txt
@@ -63,9 +59,8 @@ echo  -e "\n# Domains records\n" >> results.txt
 domains_file="filtered_domains.txt"
 output_file="results.txt"
 
-# Loop through each domain
 while read domain; do
-    echo "Performing dig lookup for ${domain}..."
+    echo -e "\n $RED Performing dig lookup for ${domain}...\n"
     dig_output="$(dig ${domain} +nocmd +nocomments +nostats)"
     echo "${dig_output}"
     echo "${dig_output}" >> "${output_file}"
@@ -75,37 +70,31 @@ done < "${domains_file}"
 getips  -v -d filtered_domains.txt -o ips.txt
 
 echo -e "\n# Unique Domains\n$(cat filtered_domains.txt)" >> results.txt
-
 echo -e "\n# IPs Alive\n$(cat ips.txt)" >> results.txt
-
 touch https_domains.txt http_ips.txt
-
-# Read the domains.txt file, append "https://" to each line, and save the results to https_domains.txt
-while read domain; do
-  echo "https://$domain" >> https_domains.txt
-done < filtered_domains.txt
 
 echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
 echo $GREEN [x] Extracting link from each website.. $RESET
 echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
 
-echo -e "\n# Links extracted\n" >> results.txt
+while read domain; do
+  echo "https://$domain" >> https_domains.txt
+done < filtered_domains.txt
+
+echo -e "\n# Links extracted from all domains\n" >> results.txt
 
 # Extracting links
 while read url; do
-    # Run dirsearch on the domain and append the output to results.txt
     echo -e "\n $RED [x] Extracting link from $url...\n" $RESET
     lynx -listonly -dump -unique_urls -connect_timeout=10 -read_timeout=15 "$url" | tee -a results.txt
 done < https_domains.txt
 
-# Read the ips.txt file, append "http://" to each line, and save the results to http_ips.txt
 while read ip; do
   echo "http://$ip" >> http_ips.txt
 done < ips.txt
 
 # Extracting links
 while read url; do
-    # Run dirsearch on the domain and append the output to results.txt
     echo -e "\n $RED [x] Extracting link from $url...\n" $RESET
     lynx -listonly -dump -unique_urls -connect_timeout=10 -read_timeout=15 "$url" | tee -a results.txt
 done < http_ips.txt
@@ -162,7 +151,7 @@ while read -r domain; do
 done < domains.txt
 
 echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
-echo $GREEN [x] Saving injection point.. $RESET
+echo $GREEN [x] Saving possible injection point.. $RESET
 echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
 
 grep "?" unique_urls.txt > injection_point.txt
@@ -170,9 +159,9 @@ grep "?" unique_urls.txt > injection_point.txt
 while true; do
   # Show menu options
   echo -e "\n $RED [x] Choose an option\n" $RESET
-  echo "1. SQLmap"
-  echo "2. Nmap"
-  echo "3. Exit"
+  echo -e "\n $GREEN 1. SQLmap\n" $RESET
+  echo -e "\n $GREEN 2. Nmap\n" $RESET
+  echo -e "\n $GREEN 3. Exit" $RESET
 
   # Ask user for choice
   read -p "Enter your choice (1-3): " choice
